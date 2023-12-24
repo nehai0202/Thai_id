6d7924446371ca3ff84fcf1b5cca9926564c6c92
nehai0202 <niharikamahajan47@gmail.com>
2023-12-24 14:37:20 +0530

    tested API using postman and earlier also added moment library to format dates

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express= require("express");
+const dotenv= require("dotenv");
 const app= express();
 
+dotenv.config();
+const PORT= process.env.PORT || 5000
 app.get("/",(req,res)=>{
-    res.send("api runing");
+    res.send("api runing compleeetely ");
 })
 
-app.listen(5000,console.log("server on port 5000"));
+app.listen(5000,console.log(`server on port  ${PORT}`));
