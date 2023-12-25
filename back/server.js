3c4bea3c70ce712c341d166ec6b5f6e9063392b3
nehai0202 <niharikamahajan47@gmail.com>
2023-12-25 21:48:28 +0530

    Added frontend and some css

---
+++ back/server.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const multer = require("multer");
+const ApiRoutes = require("./routes/userroute");
+const dotenv = require("dotenv");
+const { callAnnotateImage, OCRService } = require("./codeserv");
+//const { OCRService } = require("../codeserv/index");
+const OCRRecord = require('./model/ocr_record');
+const ocrService = new OCRService();
+
+dotenv.config();
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+const setupAndStartServer = async () => {
+  // create the express object
+  const app = express();
+
+  app.use(cors());
+  // middlewares
+  app.use(express.json());
+  app.use(bodyparser.json());
+  app.use(bodyparser.urlencoded({ extended: true }));
+
+  app.use("/api", ApiRoutes);
+
+  const upload=multer({dest:'uploads/'});
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      return cb(null, '/uploads');
+    },
+    filename: function (req, file, cb) {
+      return cb(null, `${Date.now()}_${file.originalname}`);
+    },
+  });
+ 
+
+  app.post("/api/upload", upload.single("file"), async (req, res) => {
+    try {
+      console.log(req.file);
+      const path = req.file.destination + "/" + req.file.filename;
+      console.log(path);
+  
+      // Assuming callAnnotateImage returns the result object
+      const result = await callAnnotateImage(path);
+  
+      // Assuming callAnnotateImage returns an object with properties
+      const ocrRecord = new OCRRecord({
+        name: result.name,
+        last_name: result.last_name,
+        identification_number: result.identification_number,
+        date_of_birth: result.date_of_birth,
+        date_of_issue: result.date_of_issue,
+        date_of_expiry: result.date_of_expiry,
+      });
+  
+      await ocrRecord.save();
+  
+      res.status(201).json({
+        data: ocrRecord,
+        success: true,
+        message: "Successfully processed and added a record",
+        err: {},
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        data: {},
+        success: false,
+        message: "Error in text detection service",
+        err: error,
+      });
+    }
+  });
+  
+
+  
+
+  app.get("/", (req, res) => {
+    res.send("api runing compleeetely ");
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Started server at ${PORT}`);
+  });
+};
+
+setupAndStartServer();
