789d5b81f370525c275ec09335f70bfde3b4de11
nehai0202 <niharikamahajan47@gmail.com>
2023-12-26 14:28:32 +0530

    add

--- back/server.js
+++ back/server.js
@@ -40,20 +40,20 @@ const setupAndStartServer = async () => {
     },
   });
  //-----------------deployment--------------------------------------------------------------
-   const __dirname1= path.resolve();
-   if(process.env.NODE_ENV=== "production")
-   {
-     app.use(express.static(path.join(__dirname1,"/frontend/build")));
-   app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
-   })
-   }
-   else
-   {
-     app.get("/",(req,res)=>{
-       res.send("API running successfully")
-     })
-    }
+  // const __dirname1= path.resolve();
+  // if(process.env.NODE_ENV=== "production")
+  // {
+  //   app.use(express.static(path.join(__dirname1,"/frontend/build")));
+  // app.get('*',(req,res)=>{
+  //  res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
+  // })
+  // }
+  // else
+  // {
+  //   app.get("/",(req,res)=>{
+  //     res.send("API running successfully")
+  //   })
+  //  }
 // -------------deployment----------------------------------------------------------
   app.post("/api/upload", upload.single("file"), async (req, res) => {
     try {
