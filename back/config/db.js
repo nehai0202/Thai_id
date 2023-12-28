e4d6440bbd7275ce127a9bf715bcc74c69a5082a
nehai0202 <niharikamahajan47@gmail.com>
2023-12-28 11:14:02 +0530

    add

--- back/config/db.js
+++ back/config/db.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
      
     });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+   // console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
     process.exit();
