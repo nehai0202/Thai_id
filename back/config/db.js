1c91add1f3c0d064bca8e834969507455f8f9c4b
nehai0202 <139651627+nehai0202@users.noreply.github.com>
2023-12-29 14:49:40 +0530

    Update db.js

--- back/config/db.js
+++ back/config/db.js
@@ -4,8 +4,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology:true,
-      useNewUrlParser: true,
+     
       
      
     });
